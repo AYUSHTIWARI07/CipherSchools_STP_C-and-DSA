f454997c0edc78663466a1ba3059cd85bc820a79
Ayush Tiwari <ayushtiwari.2004at@gmail.com>
2024-07-25 20:59:55 +0530

    Add files via upload

---
+++ Lecture4-CipherSchools.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  //Declaring 2 variables
+  int a;
+  int b;
+  int c;
+  int d;
+  //Assigning values to them
+  a=3;
+  b=5;
+  //Adding them both
+  c=a+b;
+  //Addng another value
+  d=a+b+c;
+  //Printing them out
+  cout<<c<<endl<<d;
+  return 0;
+}
